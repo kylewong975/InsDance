bae07e8c275347b8f6e273a49a2e7133e734dd04
kylewong975 <kyle.wong975@gmail.com>
2018-04-21 22:55:52 -0700

    Fixate particles.js as background

--- src/App.js
+++ src/App.js
@@ -6,30 +6,46 @@ import Particles from 'react-particles-js';
 export default class App extends Component {
   render() {
     return (
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 80,
-            },
-            line_linked: {
-            	shadow: {
-            		enable: true,
-            		color: "#70c1d0",
-            		blur: 10
-            	},
-              color: "#70c1d0",
-              opacity: 0.4,
-              width: 2,
-            },
-          }
-        }}
-        style={{
-          width: '100%',
-          height: "100%",
-          backgroundColor: "#1c222b"
-        }}
-      />
+      <div>
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 80,
+              },
+              line_linked: {
+              	shadow: {
+              		enable: true,
+              		color: "#70c1d0",
+              		blur: 10
+              	},
+                color: "#70c1d0",
+                opacity: 0.4,
+                width: 2,
+              },
+            }
+          }}
+          style={{
+            width: '100%',
+            height: "100%",
+            backgroundColor: "#1c222b",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            zIndex: -1,
+            marginTop: -20,
+          }}
+        />
+        <div style={styles.canvas}>
+          <p>Hello</p>
+        </div>
+      </div>
     );
+  }
+}
+
+let styles = {
+  canvas: {
+    color: "white"
   }
 }
